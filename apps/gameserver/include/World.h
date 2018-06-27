ec1ddd334ecfc5efa19ac6a06f57e1790819ff49
Damian Kalinowski <damiikalii@gmail.com>
2018-06-27 22:06:40 +0200

    Entities

--- apps/gameserver/include/World.h
+++ apps/gameserver/include/World.h
@@ -13,67 +13,99 @@ class WorldMap
 public:
     class Container : public bango::space::quad_entity_container<Container>
     {
-        std::list<Player*>    m_players;
-        std::list<NPC*>       m_npcs;
+        // std::list<Player*>    m_players;
+        // std::list<NPC*>       m_npcs;
+
+        typedef std::map<unsigned int, Character*> CharacterContainer;
+
+        std::map<char, CharacterContainer> m_entities;
 
     public:
-        const std::list<Player*>& players()   const { return m_players;   }
-        const std::list<NPC*>&    npcs()      const { return m_npcs;      }
+        // const std::list<Player*>& players()   const { return m_players;   }
+        // const std::list<NPC*>&    npcs()      const { return m_npcs;      }
+
+        Container()
+        {
+            m_entities[Character::PLAYER]   = {};
+            m_entities[Character::NPC]      = {};
+        }
+
+        const CharacterContainer& players()     const { return m_entities.at(Character::PLAYER); }
+        const CharacterContainer& npcs()        const { return m_entities.at(Character::NPC); }
 
         // TODO: Add check if entity already exists. std::list->std::map?
         void insert(const bango::space::quad_entity* entity) override
         {
             // TODO: Change?
-            switch (((Character*)entity)->GetType())
-            {
-                case Character::PLAYER: 
-                    m_players.push_back((Player*) entity);
-                    break;
-                case Character::NPC:
-                    m_npcs.push_back((NPC*) entity);
-                    break;
-            }
+            // switch (((Character*)entity)->GetType())
+            // {
+            //     case Character::PLAYER: 
+            //         m_players.push_back((Player*) entity);
+            //         break;
+            //     case Character::NPC:
+            //         m_npcs.push_back((NPC*) entity);
+            //         break;
+            // }
+            auto character = (Character*) entity;
+
+            m_entities[character->GetType()].insert(std::make_pair(character->GetID(), character));
         }
 
         // TODO: Add check if entity already exists. std::list->std::map?
         void remove(const bango::space::quad_entity* entity) override
         {
-            switch (((Character*)entity)->GetType())
-            {
-                case Character::PLAYER: 
-                    m_players.remove((Player*) entity);
-                    break;
-                case Character::NPC: 
-                    m_npcs.remove((NPC*) entity);
-                    break;
-            }
+            // switch (((Character*)entity)->GetType())
+            // {
+            //     case Character::PLAYER: 
+            //         m_players.remove((Player*) entity);
+            //         break;
+            //     case Character::NPC: 
+            //         m_npcs.remove((NPC*) entity);
+            //         break;
+            // }
+            auto character = (Character*) entity;
+
+            m_entities[character->GetType()].erase(character->GetID());
         }
 
         void merge(const Container* container) override
         {
-            m_players   .insert(m_players.end(),    container->m_players.begin(),   container->m_players.end()  );
-            m_npcs      .insert(m_npcs.end(),       container->m_npcs.begin(),      container->m_npcs.end()     );
+            // m_players   .insert(m_players.end(),    container->m_players.begin(),   container->m_players.end()  );
+            // m_npcs      .insert(m_npcs.end(),       container->m_npcs.begin(),      container->m_npcs.end()     );
+            for (const auto& p : container->m_entities)
+                m_entities[p.first].insert(p.second.cbegin(), p.second.cend());
         }
 
         size_t size() const override
         {
-            return m_players.size() + m_npcs.size();
+            size_t count=0;
+            for (const auto& p : m_entities)
+                count += p.second.size();
+            return count;
+            //return m_players.size() + m_npcs.size();
         }
 
         long long total_memory() const override
         {
-            return 
-                sizeof(m_players)+m_players.size()*sizeof(Player*) +
-                sizeof(m_npcs)+m_npcs.size()*sizeof(NPC*);
+            // return 
+            //     sizeof(m_players)+m_players.size()*sizeof(Player*) +
+            //     sizeof(m_npcs)+m_npcs.size()*sizeof(NPC*);
+            long long count=0;
+            for (const auto& p : m_entities)
+                count += sizeof(p.second)+p.second.size()*sizeof(Character*);
         }
         
         void for_each(const std::function<void(const bango::space::quad_entity*)>&& callback) const override
         {
-            for (auto& qe : m_players)
-                callback(qe);
+            // for (auto& qe : m_players)
+            //     callback(qe);
 
-            for (auto& qe : m_npcs)
-                callback(qe);
+            // for (auto& qe : m_npcs)
+            //     callback(qe);
+
+            for (const auto& p : m_entities)
+                for (auto& q : p.second)
+                    callback(q.second);
         }
     };
 
@@ -112,8 +144,10 @@ public:
     {
         auto center = bango::space::point{entity->m_x, entity->m_y};
         m_quad.query(center, m_sight, [&](const Container* container) {
-            for (auto& player : container->players())
+            for (auto& p : container->players())
             {
+                auto player = (Player*) p.second;
+
                 if (player->distance(center) <= m_sight)
                 {
                     m_on_appear(player, entity);
@@ -125,8 +159,10 @@ public:
             if (entity->GetType() != Character::PLAYER)
                 return;
 
-            for (auto& npc : container->npcs())
+            for (auto& p : container->npcs())
             {
+                auto npc = p.second;
+
                 if (npc->distance(center) <= m_sight)
                     m_on_appear((Player*) entity, npc);
             }
@@ -171,8 +207,10 @@ public:
 
         auto center = bango::space::point{entity->m_x, entity->m_y}; // TODO: Dont convert to point each time.
         m_quad.query(center, m_sight, [&](const Container* container) {
-            for (auto& player : container->players())
+            for (auto& p : container->players())
             {
+                auto player = (Player*) p.second;
+
                 if (player->distance(center) <= m_sight)
                     m_on_disappear(player, entity);
             }
@@ -199,8 +237,10 @@ public:
 
         // TODO: Add some margin.
         m_quad.query(old_center, m_sight, [&](const Container* container) {
-            for (auto& player : container->players())
+            for (auto& p : container->players())
             {
+                auto player = (Player*) p.second;
+
                 if (player->distance(old_center) <= m_sight && player->distance(new_center) > m_sight) 
                 {
                     m_on_disappear(player, entity);
@@ -212,16 +252,20 @@ public:
             if (entity->GetType() != Character::PLAYER)
                 return;
 
-            for (auto& npc : container->npcs())
+            for (auto& p : container->npcs())
             {
+                auto npc = p.second;
+
                 if (npc->distance(old_center) <= m_sight && npc->distance(new_center) > m_sight) 
                     m_on_disappear((Player*) entity, npc);
             }
         });
 
         m_quad.query(new_center, m_sight, [&](const Container* container) {
-            for (auto& player : container->players())
+            for (auto& p : container->players())
             {
+                auto player = (Player*) p.second;
+
                 if (player->distance(old_center) <= m_sight && player->distance(new_center) <= m_sight) 
                     m_on_move(player, entity, delta_x, delta_y, delta_z, stop);
                 else if (player->distance(old_center) > m_sight && player->distance(new_center) <= m_sight) 
@@ -235,8 +279,10 @@ public:
             if (entity->GetType() != Character::PLAYER)
                 return;
 
-            for (auto& npc : container->npcs())
+            for (auto& p : container->npcs())
             {
+                auto npc = p.second;
+
                 if (npc->distance(old_center) > m_sight && npc->distance(new_center) <= m_sight) 
                     m_on_appear((Player*) entity, npc);
             }
@@ -253,7 +299,9 @@ public:
     {
         auto center = bango::space::point{qe->m_x, qe->m_y};
         m_quad.query(center, radius, [&](const Container* container) {
-            for (auto& player : container->players()) {
+            for (auto& p : container->players()) {
+                auto player = (Player*) p.second;
+
                 if (player->distance(center) <= radius)
                     callback(player);
             }
@@ -282,7 +330,6 @@ class World
 {
     constexpr static unsigned short MAP_COUNT = 32;
 
-    // BUG: Stack vs heap
     std::vector<std::unique_ptr<WorldMap>> m_maps;
 
     std::map<char, std::map<unsigned int, Character*>> m_entities;
