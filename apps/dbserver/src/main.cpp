64f984145909da51c45fc540319cd3dd024d58d5
Damian <damiikalii@gmail.com>
2022-10-05 22:39:51 +0200

    creds

--- apps/dbserver/src/main.cpp
+++ apps/dbserver/src/main.cpp
@@ -6,7 +6,7 @@ int main()
 
     manager.Initialize();
 
-    manager.ConnectToPool("localhost", "3301", "root", ";", "kalonline");
+    manager.ConnectToPool("localhost", "3301", "root", "pass", "kalonline");
     manager.StartDBServer("localhost", 2999);
 
     std::cin.get();
