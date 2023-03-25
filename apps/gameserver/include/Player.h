544d595dafb02dc3d28e4e7e314591f9ad17e30d
ArslaN <114784835+ArslaAnN@users.noreply.github.com>
2023-03-25 23:27:40 +0200

    Update Player.h

--- apps/gameserver/include/Player.h
+++ apps/gameserver/include/Player.h
@@ -163,4 +163,5 @@ public:
     bool CanReciveExp();
     std::uint64_t CalculateExp(std::uint64_t exp, std::uint8_t monster_level);
     void LevelUp();
+    void PrivateNotice(std::string message);
 };
